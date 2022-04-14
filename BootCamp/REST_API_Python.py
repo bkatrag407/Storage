48bddb3e909a689c09ed07fcce52ab6a8c2b9db4
Bala <balavinodhk@gmail.com>
2022-04-13 22:18:33 -0500

    test

--- BootCamp/REST_API_Python.py
+++ BootCamp/REST_API_Python.py
@@ -1,6 +1,12 @@
 import requests
 
 r=requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-4-11&to=2022-4-12&sortBy=popularity&language=en&apiKey=890603a55bfa47048e4490069ebee18c')
+print(type(r))
 output=r.json()
-#print(type(output))
-print(output['articles'][0]['title'])
+print(type(output))
+#print(output['articles'])
+articles = output['articles']
+#print(articles)
+
+for article in articles:
+    print(article['title'])
