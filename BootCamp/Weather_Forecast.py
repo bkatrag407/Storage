d4c34c7ecdd4c8c1ce337884df9d1bd36a10292b
Bala <balavinodhk@gmail.com>
2022-04-13 23:26:35 -0500

    final

---
+++ BootCamp/Weather_Forecast.py
@@ -0,0 +1,13 @@
+import requests
+
+def weather_forecast():
+    url='https://api.openweathermap.org/data/2.5/forecast?q=celina&appid=989f9145125504f3fa2a7a22b49ea2d1'
+    weather=requests.get(url)
+    output=weather.json()
+    items=output['list']
+    city=output['city']['name']
+    print('City,Time,Temparature,Condition')
+    for item in items:
+        print(city,',',item['dt_txt'],',',item['main']['temp'],',',item['weather'][0]['description'])
+
+weather_forecast()
